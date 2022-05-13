698f04e37da62e462ad8735086edc51178f2d7c5
EduardoRdz007 <edurdz010@gmail.com>
2022-05-12 20:39:37 -0500

    chore: add comit

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ function doSomething(){
     console.log("Hello World")
 }
 
-<<<<<<< HEAD
 function develop(){
     console.log("Rama develop")
-=======
+}
 function doSomething(){
     console.log("Hello Testing")
->>>>>>> testing
 }
