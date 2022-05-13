77610a021928f2842b94cc9b03bc0a6993622dae
EduardoRdz007 <edurdz010@gmail.com>
2022-05-12 20:00:41 -0500

    Guardado

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ function doSomething(){
     console.log("Hello World")
 }
 
+<<<<<<< Updated upstream
+=======
+function develop(){
+    console.log("Rama develop")
+}
+
+>>>>>>> Stashed changes
 function doSomething(){
     console.log("Hello Testing")
 }
